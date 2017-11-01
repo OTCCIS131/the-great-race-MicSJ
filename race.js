fcf30bbb97dc50f1a93bd39216a1cfe690caa1f8
Unknown <mj0857363@otc.edu>
2017-10-31 22:28:35 -0500

    display winner

--- race.js
+++ race.js
@@ -5,11 +5,13 @@ var startButton = new Vue({
         tina: 0,
         louise: 0,
         interval: 0,
+        tin: null,
+        lou: null,
     },
 
     methods: {
         startRace: function (event) {
-            this.interval = setInterval(this.progressPlayers, 800)
+            this.interval = setInterval(this.progressPlayers, 400)
         },
 
         progressPlayers: function () {
@@ -21,8 +23,25 @@ var startButton = new Vue({
         },
 
         checkVictory: function () {
-            if (this.tina >= 80 || this.louise >= 80)
+            if (this.tina >= 80 || this.louise >= 80) {
                 clearInterval(this.interval)
+                this.declareWin()
+            }
+        },
+
+        declareWin: function () {
+            if (this.tina > this.louise) {
+                document.getElementById("tina").style.left = "40vw"
+                document.getElementById("louise").style.visibility = "hidden"
+            }
+            else {
+                document.getElementById("louise").style.left = "40vw"
+                document.getElementById("tina").style.visibility = "hidden"
+            }
+        },
+
+        reset: function () {
+
         },
     },
 
