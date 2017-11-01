dc61c5ec68fdbf95727adfd12a7c515513085840
Unknown <mj0857363@otc.edu>
2017-10-31 21:47:19 -0500

    It moves!

--- race.js
+++ race.js
@@ -1,29 +1,37 @@
 var startButton = new Vue({
-    el: '#startButton',
+    el: '.startButton',
 
     data: {
         tina: 0,
         gene: 0,
+        interval: 0,
     },
 
     methods: {
         startRace: function (event) {
-            setInterval(this.progressPlayers, 1000)
+            this.interval = setInterval(this.progressPlayers, 800)
         },
 
         progressPlayers: function () {
-            debugger
             this.tina += Math.floor(Math.random() * 5) + 1
             this.gene += Math.floor(Math.random() * 5) + 1
+            this.tinaStyles
+            this.geneStyles
+            this.checkVictory
         },
 
+        checkVictory: function () {
+            clearInterval(this.interval)
+        },
     },
 
     computed: {
-        tinaStyles() {
-            return {
-                left: $(this.tina) + 'vw'
-            }
+        tinaStyles: function () {
+            document.getElementById("tina").style.left = this.tina + "vw"
+        },
+
+        geneStyles: function () {
+            document.getElementById("gene").style.left = this.gene + "vw"
         },
     },
 })
